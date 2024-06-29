a76d1ace7c84b110b1211b2616cbc6412efec075
43a1723 <hai1723notgay@gmail.com>
2024-06-29 16:39:32 +0700

    Update uacbypass.ps1

--- file/uacbypass.ps1
+++ file/uacbypass.ps1
@@ -1,1 +1,16 @@
+function Get-CommandPath {
+    if ($MyInvocation.MyCommand.Path) {
+        # Nếu $MyInvocation.MyCommand.Path có giá trị, trả về giá trị của nó
+        return $MyInvocation.MyCommand.Path
+    } else {
+        # Nếu $MyInvocation.MyCommand.Path không có giá trị, lấy đường dẫn của tiến trình hiện tại
+        $path = (Get-Process -Id $PID).Path
+        return $path
+    }
+}
 
+# Gọi hàm và lưu kết quả vào biến
+$path = Get-CommandPath
+
+# In giá trị của biến $path
+Write-Output $path
